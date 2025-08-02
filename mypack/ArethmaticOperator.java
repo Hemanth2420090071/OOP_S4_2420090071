08c154c86bb87d6fcf51c2ff4d2e75c24ad7e9a1
Hemanth2420090071 <2420090071@klh.edu.in>
2025-08-02 11:15:13 +0530

    ArethmaticOperator

---
+++ mypack/ArethmaticOperator.java
@@ -0,0 +1,18 @@
+package mypack;
+
+import java.util.*;
+
+public class ArithmeticOperators {
+	
+	 public static void main(String[] args) {
+		  System.out.println("Enter two numbers");
+		 Scanner sc = new Scanner();
+		 int a = sc.nextInt();
+		 int b = sc.nextInt();
+		 int c = a+b;
+		 System.out.println(c);
+		 
+		 
+	 }  
+
+}
