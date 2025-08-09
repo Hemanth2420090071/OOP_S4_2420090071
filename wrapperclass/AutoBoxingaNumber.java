7a646f05f5584c7e1bdae233a9804c608dacf781
Hemanth2420090071 <2420090071@klh.edu.in>
2025-08-09 11:24:02 +0530

    Add files via upload

---
+++ wrapperclass/AutoBoxingaNumber.java
@@ -0,0 +1,15 @@
+package wrapperclass;
+import java.util.*;
+public class AutoBoxingaNumber {
+     
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter an int value: ");
+        int num = sc.nextInt();
+
+        Integer obj = num;
+        System.out.println("Auto-boxed Integer: " + obj);
+    }
+}
+
---
+++ wrapperclass/AutoUnboxingaNumber.java
@@ -0,0 +1,14 @@
+package wrapperclass;
+import java.util.*;
+public class AutoUnboxingaNumber {
+    
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter an Integer value: ");
+        int input = sc.nextInt();
+        Integer obj = input;
+        int num = obj;
+        System.out.println("Auto-unboxed int: " + num);
+    }
+}
+
